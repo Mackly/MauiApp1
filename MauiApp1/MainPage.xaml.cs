7ba345ef0894a00896acc78a80a98b9cc1485594
Mackly Férère-Antoine <macklyfantoine@gmail.com>
2022-11-09 19:51:58 -0500

    Added Custom WebViewClient Mapped Custom WebViewClient

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,14 +1,25 @@
-﻿namespace MauiApp1;
+﻿using Microsoft.Maui.Handlers;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+#if ANDROID
+        Microsoft.Maui.Handlers.WebViewHandler.Mapper.ModifyMapping(nameof(Android.Webkit.WebViewClient), (handler, view, action) =>
+        {
+            MapCustomWebViewClient(handler, view);
+        });
+#endif
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-	}
+#if ANDROID
+    public void MapCustomWebViewClient(IWebViewHandler handler, IWebView webView)
+    {
+        handler.PlatformView.SetWebViewClient(new MauiApp1.Platforms.Android.CustomWebViewClient(handler as WebViewHandler));
+    }
+#endif
 }
 
---
+++ MauiApp1/Platforms/Android/CustomWebViewClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.Webkit;
+
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace MauiApp1.Platforms.Android
+{
+    class CustomWebViewClient : MauiWebViewClient
+    {
+        public CustomWebViewClient(WebViewHandler handler) : base(handler)
+        {
+
+        }
+    }
+}
